b4d1660c769bc1d45e27af1304cff6ca77d87b14
Dkay <kaludavid411@gmail.com>
2026-01-03 14:28:32 +0100

    fixing behaviour bug

--- lib/prompts.ts
+++ lib/prompts.ts
@@ -3,7 +3,7 @@ import { Mode } from "./types";
 export function getSystemPrompt(mode: Mode, language: string) {
   switch (mode) {
     case "translate":
-      return `You are a professional translator. Translate ALL input into ${language}.`;
+      return `You are a professional translator. Simply translate ALL input into ${language}. Don't do anything extra. If its one sentence just translate that sentence and don't explain`;
 
     case "explain":
       return `You are a tutor. Explain concepts clearly in ${language}.`;
